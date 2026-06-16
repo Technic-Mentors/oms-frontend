7d6f988fd9c88c69547b8e80d1d1b38f363103af
Hamza Amin <hamzaamin104@gmail.com>
2026-06-16 10:10:06 +0500

    innitialgr

--- src/Content/URL.tsx
+++ src/Content/URL.tsx
@@ -1,5 +1,5 @@
 // export const BASE_URL = "http://localhost:3001";
 
-export const BASE_URL = "https://mydesk-backend.vercel.app";
+export const BASE_URL = "https://mydesk-backend.vercel.app/";
 
 
